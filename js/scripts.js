d5bb0757e879a8da8d9671da5d461d6542db516e
marcelklopper <66620387+marcelklopper@users.noreply.github.com>
2023-03-06 22:50:35 +0200

    calibaby

--- js/scripts.js
+++ js/scripts.js
@@ -196,35 +196,35 @@ $(document).ready(function () {
     });
 
     /********************** Add to Calendar **********************/
-    var myCalendar = createCalendar({
-        options: {
-            class: '',
-            // You can pass an ID. If you don't, one will be generated for you
-            id: ''
-        },
-        data: {
-            // Event title
-            title: "Carelize&Marcel's Wedding",
-
-            // Event start date
-            start: new Date('Dec 17, 2023 10:00'),
-
-            // Event duration (IN MINUTES)
-            // duration: 120,
-
-            // You can also choose to set an end time
-            // If an end time is set, this will take precedence over duration
-            end: new Date('Dec 19, 2023 10:00'),
-
-            // Event Address
-            address: 'Elandsfloof Farm Cottages',
-
-            // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please get in touch"
-        }
-    });
-
-    $('#add-to-cal').html(myCalendar);
+    // var myCalendar = createCalendar({
+    //     options: {
+    //         class: '',
+    //         // You can pass an ID. If you don't, one will be generated for you
+    //         id: ''
+    //     },
+    //     data: {
+    //         // Event title
+    //         title: "Carelize&Marcel's Wedding",
+    //
+    //         // Event start date
+    //         start: new Date('Dec 17, 2023 10:00'),
+    //
+    //         // Event duration (IN MINUTES)
+    //         // duration: 120,
+    //
+    //         // You can also choose to set an end time
+    //         // If an end time is set, this will take precedence over duration
+    //         end: new Date('Dec 19, 2023 10:00'),
+    //
+    //         // Event Address
+    //         address: 'Elandskloof Farm Cottages',
+    //
+    //         // Event Description
+    //         description: "We can't wait to see you on our big day. For any queries or issues, please get in touch"
+    //     }
+    // });
+    //
+    // $('#add-to-cal').html(myCalendar);
 
 
     /********************** RSVP **********************/
